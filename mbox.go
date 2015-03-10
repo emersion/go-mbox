918c9c8680d9379dd52c8584a184b62aa69ea339
Tobias Rehbein <tobias.rehbein@web.de>
2015-03-10 19:59:03 +0100

    Add license

--- mbox.go
+++ mbox.go
@@ -1,3 +1,9 @@
+// "THE BEER-WARE LICENSE" (Revision 42):
+// <tobias.rehbein@web.de> wrote this file. As long as you retain this notice
+// you can do whatever you want with this stuff. If we meet some day, and you
+// think this stuff is worth it, you can buy me a beer in return.
+//                                                             Tobias Rehbein
+
 // Package mbox parses mbox files into messages.
 //
 // As the mbox file format is not standardized this package expects the least
